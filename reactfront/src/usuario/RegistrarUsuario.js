a7764fcfa8de859b4ee974b04a519b10ff9daf59
Aacini++ <azambrano@cyberideas.com.mx>
2022-04-28 14:46:26 -0500

    formulario registrarse

--- reactfront/src/usuario/RegistrarUsuario.js
+++ reactfront/src/usuario/RegistrarUsuario.js
@@ -38,7 +38,7 @@ const CompRegistrarUsuario=()=>{
        
    }
 
-
+   const [ContarHistoria, setContarHistoria] = useState(false)
 
     const [Nombre, setNombre] = useState('')
     const [NickName, setNickName] = useState('')
@@ -54,6 +54,9 @@ const CompRegistrarUsuario=()=>{
     const [PerfilFB, setPerfilFB] = useState(undefined)
     const [Email, setEmail] = useState(undefined)
     const [Telefono, setTelefono] = useState(undefined)
+
+    const [Descripcion, setDescripcion] = useState(undefined)
+
 
     const navigate=useNavigate();
 
@@ -74,7 +77,14 @@ const CompRegistrarUsuario=()=>{
             Email: (Email == "")? null : Email ,
             Telefono:(Telefono == "")? null : Telefono, 
         } 
+
+        if(ContarHistoria){
+            params["susTickets" ]= [ { Descripcion:Descripcion}]
+            
+        }
         
+        
+
         console.log(params)
         
         
@@ -96,170 +106,211 @@ const CompRegistrarUsuario=()=>{
 
 
     return (
-        <div >
-        <h3>Registrar Usuaria</h3>
+        <div className="container-fluid" >
+        <h3>Crear Cuenta</h3>
 
         <div className="alert alert-success" role="alert">{msgEstado}</div>
         <div className="alert alert-danger" role="alert">{msgError}</div>
 
-        <form onSubmit={store}>
+        <form className="row  justify-content-center" onSubmit={store}>
 
-            <div className="row">
-                <div className="form-group col-md-6">
+            <div className="col-12 col-md-10">
+                <div className="row align-items-center"  >
+                    <div className="col-12 col-md-6">
+                        
+                                <div className="row">
+                                    <div className="form-group col-md-6">
 
-                    <label className="form-label required" >NickName</label>
-                    <input value={NickName}
-                    onChange={  (e)=> setNickName(e.target.value)  }
-                    type="text"
-                    className="form-control"
-                    required    
-                    />
+                                        <label className="form-label required" >NickName</label>
+                                        <input value={NickName}
+                                        onChange={  (e)=> setNickName(e.target.value)  }
+                                        type="text"
+                                        className="form-control"
+                                        required    
+                                        />
+                                
+                                            
+
+                                    </div>
+                                    <div className="form-group col-md-6">
+                                        
+                                            <label className="form-label required" >Pass</label>
+                                            <input value={Pass}
+                                            onChange={  (e)=> setPass(e.target.value)  }
+                                            type="password"
+                                            className="form-control"
+                                            required    
+                                            />
+                                    
+
+                                    </div>
+                                </div>
+
+
+                                <div className="mb-3">
+                                    <label className="form-label required" >Nombre (s)</label>
+                                        <input 
+                                        value={Nombre}
+                                        onChange={  (e)=> setNombre(e.target.value)  }
+                                        type="text"
+                                        className="form-control"
+                                        required    
+                                        />
+                                </div>
+
+
+
+                                <div className="row align-items-end">
+                                    <div className="form-group col-md-6">
+                                        
+                                        <label className="form-label" >Apellido Paterno</label>
+                                        <input 
+                                        value={ApellidoPaterno}
+                                        onChange={  (e)=> setApellidoPaterno(e.target.value)  }
+                                        type="text"
+                                        className="form-control"    
+                                        />
+
+
+                                    </div>
+                                    <div className="form-group col-md-6">
+                                        
+                                        <label className="form-label" >Apellido Materno</label>
+                                        <input 
+                                        value={ApellidoMaterno}
+                                        onChange={  (e)=> setApellidoMaterno(e.target.value)  }
+                                        type="text"
+                                        className="form-control"    
+                                        />
+
+
+
+                                    </div>
+                                </div>
+
+                            
+                            
+
+                                <div className="row align-items-end">
+                                        <div className="form-group col-md-6">
+                                            
+                                                <label className="form-label" >Email</label>
+                                                <input 
+                                                value={Email}
+                                                onChange={  (e)=> setEmail(e.target.value)  }
+                                                type="email"
+                                                placeholder="Ej. ejemplo@correo.com"
+                                                className="form-control"    
+                                                />
+
+
+                                        </div>
+                                        <div className="form-group col-md-6">
+                                            
+                                                <label className="form-label" >Celular</label>
+                                                <input value={Telefono}
+                                                onChange={  (e)=> setTelefono(e.target.value)  }
+                                                type="number"
+                                                placeholder="Ej: 55 1234 56789"
+                                                className="form-control"    
+                                                />
+
+
+                                        </div>
+                                </div>
+
+                                <div className="mb-3">
+
+                                <label className="form-label" >Perfil de Fb</label>
+                                <input value={PerfilFB}
+                                onChange={  (e)=> setPerfilFB(e.target.value)  }
+                                type="text"
+                                className="form-control"
+                                placeholder="Ej: fb.com/alguien"
+                                    
+                                />
+                                </div>
+
+
+
+
+                                <div className="row align-items-end">
+                                        <div className="form-group col-md-6">
+                                            
+                                                <label className="form-label"    >Estado</label>
+                                                <select name="Estado" className="form-select"  onChange={  (e)=> {setEstado(e.target.value)  }  }>  
+                                                <option value="34"  > Seleccione un estado.. </option>  
+                                                    { estados.map( (edo)=>(
+                                                        <option key={ edo.id} value={edo.id} > {edo.Estado}  </option>
+                                                    ))}
+
+                                                </select>  
+
+
+                                        </div>
+                                        <div className="form-group col-md-6">
+                                            
+                                                <label className="form-label" >Ciudad</label>
+                                                <input value={Ciudad}
+                                                onChange={  (e)=> setCiudad(e.target.value)  }
+                                                type="text"
+                                                className="form-control"    
+                                                />
+
+
+                                        </div>
+                                </div>
+
+                                <div className="mb-3">
+
+                                                <label className="form-label" >Fecha de Nacimiento</label>
+                                                <input 
+                                                value={FechaNacimiento}
+                                                onChange={  (e)=> setFechaNacimiento(e.target.value)  }
+                                                type="date"
+                                                className="form-control"    
+                                                />
+                                </div>
             
+                    </div>
+
+            {/* fin primer columna */}
+
+                    <div className="col-12 col-md-6">
+                        <div className="mb-3">
+                            <label className="form-label" value={ContarHistoria} >
+                                <input type="checkbox" onClick={ ()=>setContarHistoria(!ContarHistoria) }></input> 
+                                Quiero contar mi historia
+                        </label>
+                            
+                        </div>                    
                         
 
-                </div>
-                <div className="form-group col-md-6">
-                    
-                        <label className="form-label required" >Pass</label>
-                        <input value={Pass}
-                        onChange={  (e)=> setPass(e.target.value)  }
-                        type="password"
-                        className="form-control"
-                        required    
-                        />
-                   
+                        <div className="mb-3">
+                            
+                                    <textarea 
+                                 value={Descripcion}
+                                onChange={  (e)=> setDescripcion(e.target.value)  } 
+                                type="textarea"
+                                className="form-control"
+                                rows="15"
+                                placeholder="Describa la situacion por la que requiera ayuda"
+                                disabled={!ContarHistoria}
+                                onClick={ ()=>{setContarHistoria(true); console.log("cambiandoestado")} }    /* no funcionapor que en disable se desactiva */  
+                                />
+                        </div> 
 
+
+                    </div>                
                 </div>
+
             </div>
 
 
-            <div className="mb-3">
-                   <label className="form-label required" >Nombre</label>
-                    <input 
-                    value={Nombre}
-                    onChange={  (e)=> setNombre(e.target.value)  }
-                    type="text"
-                    className="form-control"
-                    required    
-                    />
+            <div className="col-12 col-md-6">
+                 <button type="submit" className="btn btn-primary" > <i className="fa-solid fa-floppy-disk"></i>Guardar </button>
             </div>
-
-
-
-            <div className="row align-items-end">
-                <div className="form-group col-md-6">
-                    
-                    <label className="form-label" >Apellido Paterno</label>
-                    <input 
-                    value={ApellidoPaterno}
-                    onChange={  (e)=> setApellidoPaterno(e.target.value)  }
-                    type="text"
-                    className="form-control"    
-                    />
-
-
-                </div>
-                <div className="form-group col-md-6">
-                    
-                    <label className="form-label" >Apellido Materno</label>
-                    <input 
-                    value={ApellidoMaterno}
-                    onChange={  (e)=> setApellidoMaterno(e.target.value)  }
-                    type="text"
-                    className="form-control"    
-                    />
-
-
-
-                </div>
-            </div>
-
-           
-           
-
-        <div className="row align-items-end">
-                <div className="form-group col-md-6">
-                    
-                        <label className="form-label" >Email</label>
-                        <input 
-                        value={Email}
-                        onChange={  (e)=> setEmail(e.target.value)  }
-                        type="email"
-                        placeholder="Ej. ejemplo@correo.com"
-                        className="form-control"    
-                        />
-
-
-                </div>
-                <div className="form-group col-md-6">
-                    
-                        <label className="form-label" >Celular</label>
-                        <input value={Telefono}
-                        onChange={  (e)=> setTelefono(e.target.value)  }
-                        type="number"
-                        placeholder="Ej: 55 1234 56789"
-                        className="form-control"    
-                        />
-
-
-                </div>
-        </div>
-
-        <div className="mb-3">
-
-           <label className="form-label" >Perfil de Fb</label>
-           <input value={PerfilFB}
-           onChange={  (e)=> setPerfilFB(e.target.value)  }
-           type="text"
-           className="form-control"
-           placeholder="Ej: fb.com/alguien"
-               
-           />
-        </div>
-
-
-
-
-        <div className="row align-items-end">
-                <div className="form-group col-md-6">
-                    
-                        <label className="form-label"    >Estado</label>
-                        <select name="Estado" className="form-select"  onChange={  (e)=> {setEstado(e.target.value)  }  }>  
-                        <option value="34"  > Seleccione un estado.. </option>  
-                            { estados.map( (edo)=>(
-                                <option key={ edo.id} value={edo.id} > {edo.Estado}  </option>
-                             ))}
-
-                        </select>  
-
-
-                </div>
-                <div className="form-group col-md-6">
-                    
-                        <label className="form-label" >Ciudad</label>
-                        <input value={Ciudad}
-                        onChange={  (e)=> setCiudad(e.target.value)  }
-                        type="text"
-                        className="form-control"    
-                        />
-
-
-                </div>
-        </div>
-
-        <div className="mb-3">
-
-                         <label className="form-label" >Fecha de Nacimiento</label>
-                        <input 
-                        value={FechaNacimiento}
-                        onChange={  (e)=> setFechaNacimiento(e.target.value)  }
-                        type="date"
-                        className="form-control"    
-                        />
-        </div>
-           
-
-            <button type="submit" className="btn btn-primary" > <i className="fa-solid fa-floppy-disk"></i> Registrar Usuaria </button>
+            
         </form>
 
 
